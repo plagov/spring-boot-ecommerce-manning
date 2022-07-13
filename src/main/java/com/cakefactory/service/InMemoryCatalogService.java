927fdfa02fdbb29def892c29c88b6bd66fe25515
Vitali Plagov <vplagov@pm.me>
2022-07-13 07:44:30 +0300

    Rename catalog service as per project's workflow

--- src/main/java/com/cakefactory/service/InMemoryCatalogService.java
+++ src/main/java/com/cakefactory/service/InMemoryCatalogService.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Component
-public class CatalogServiceImpl implements CatalogService{
+public class InMemoryCatalogService implements CatalogService{
 
     @Override
     public Iterable<Item> getItems() {
