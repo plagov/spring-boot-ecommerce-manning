0111f9504ecefd3fac28f531ecef2492a72e6228
Vitali Plagov <vplagov@pm.me>
2022-08-26 14:09:58 +0000

    Add Basket view
    Create a view to show items added to the basket. The UI part is very
    primitive. It is made just for the beginning and will improve later.

--- src/main/java/com/cakefactory/service/Basket.java
+++ src/main/java/com/cakefactory/service/Basket.java
@@ -1,8 +1,14 @@
 package com.cakefactory.service;
 
+import com.cakefactory.model.BasketItem;
+
+import java.util.List;
+
 public interface Basket {
 
     void addItem(String sku);
 
     int getBasketItemsCount();
+
+    List<BasketItem> getBasketItems();
 }
