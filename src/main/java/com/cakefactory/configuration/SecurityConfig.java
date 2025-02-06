a6bfcf332415909cfcf8697df88ef8cc9ed5b1e1
Vitali Plagov <vplagov@pm.me>
2025-02-07 00:07:30 +0200

    Encode password when registering a user

--- src/main/java/com/cakefactory/configuration/SecurityConfig.java
+++ src/main/java/com/cakefactory/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.cakefactory.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +35,9 @@ public class SecurityConfig {
         return http
                 .authorizeRequests()
                 .antMatchers("/", "/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
                 .and().build();
     }
 }
--- src/main/java/com/cakefactory/service/JpaAccountDetailsService.java
+++ src/main/java/com/cakefactory/service/JpaAccountDetailsService.java
@@ -3,20 +3,27 @@ package com.cakefactory.service;
 import com.cakefactory.model.AccountDetails;
 import com.cakefactory.model.AccountDetailsEntity;
 import com.cakefactory.repository.AccountDetailsRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JpaAccountDetailsService implements AccountDetailsService {
 
     private final AccountDetailsRepository accountDetailsRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public JpaAccountDetailsService(AccountDetailsRepository accountDetailsRepository) {
+    public JpaAccountDetailsService(
+            AccountDetailsRepository accountDetailsRepository,
+            PasswordEncoder passwordEncoder) {
         this.accountDetailsRepository = accountDetailsRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void saveAccountDetails(AccountDetails accountDetails) {
-        var entity = new AccountDetailsEntity(accountDetails.email(), accountDetails.password());
-        accountDetailsRepository.save(entity);
+        accountDetailsRepository.save(
+                new AccountDetailsEntity(
+                        accountDetails.email(),
+                        passwordEncoder.encode(accountDetails.password())));
     }
 }
