293f88185bd4d756938869b6b12c47896b787507
Vitali Plagov <vplagov@pm.me>
2025-02-07 00:27:10 +0200

    Log the user automatically after signup

--- src/main/java/com/cakefactory/service/JpaAccountDetailsService.java
+++ src/main/java/com/cakefactory/service/JpaAccountDetailsService.java
@@ -3,8 +3,12 @@ package com.cakefactory.service;
 import com.cakefactory.model.AccountDetails;
 import com.cakefactory.model.AccountDetailsEntity;
 import com.cakefactory.repository.AccountDetailsRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class JpaAccountDetailsService implements AccountDetailsService {
@@ -25,5 +29,9 @@ public class JpaAccountDetailsService implements AccountDetailsService {
                 new AccountDetailsEntity(
                         accountDetails.email(),
                         passwordEncoder.encode(accountDetails.password())));
+
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(accountDetails.email(), null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
     }
 }
