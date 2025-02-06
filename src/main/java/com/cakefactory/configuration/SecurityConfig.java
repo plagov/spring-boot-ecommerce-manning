4ee01afb459bdea4b3c5a4bdaf93edd0666a49af
Vitali Plagov <vplagov@pm.me>
2025-02-07 01:17:01 +0200

    Add login page and make possible to login

--- src/main/java/com/cakefactory/configuration/SecurityConfig.java
+++ src/main/java/com/cakefactory/configuration/SecurityConfig.java
@@ -1,16 +1,19 @@
 package com.cakefactory.configuration;
 
-import com.cakefactory.model.User;
-import com.cakefactory.repository.UserRepository;
+import com.cakefactory.model.AccountDetailsEntity;
+import com.cakefactory.repository.AccountDetailsRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -21,14 +24,20 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserRepository repository) {
-        return username -> {
-            User user = repository.findByUsername(username);
-            if (user != null) return user;
+    public UserDetailsService userDetailsService(AccountDetailsRepository repository) {
+        return email -> {
+            AccountDetailsEntity account = repository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("User with email %s not found".formatted(email)));
 
-            throw new UsernameNotFoundException("User " + username + " not found");
+
+            return new User(
+                    account.getEmail(),
+                    account.getPassword(),
+                    List.of(new SimpleGrantedAuthority("ROLE_USER"))
+            );
         };
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
