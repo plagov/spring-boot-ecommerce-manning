7f31868c8a3a74b568ce5ac82f703133537df007
Vitali Plagov <vplagov@pm.me>
2025-02-17 23:11:36 +0200

    Show delivery address of logged-in user

--- src/main/java/com/cakefactory/controller/BasketController.java
+++ src/main/java/com/cakefactory/controller/BasketController.java
@@ -1,5 +1,6 @@
 package com.cakefactory.controller;
 
+import com.cakefactory.service.AccountAddressService;
 import com.cakefactory.service.Basket;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +8,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
 import java.util.Map;
 
 @Controller
 public class BasketController {
 
     private final Basket basket;
+    private final AccountAddressService addressService;
 
-    public BasketController(Basket basket) {
+    public BasketController(Basket basket, AccountAddressService addressService) {
         this.basket = basket;
+        this.addressService = addressService;
     }
 
     @PostMapping("/basket")
@@ -25,9 +29,17 @@ public class BasketController {
     }
 
     @GetMapping("/basket")
-    public ModelAndView getBasketItems(Map<String, Object> model) {
+    public ModelAndView getBasketItems(Map<String, Object> model, Principal principal) {
         var basketItems = basket.getBasketItems();
         model.put("basketItems", basketItems);
+
+        if (principal != null) {
+            var accountAddress = addressService.getAccountAddress(principal.getName());
+            model.put("addressLine1", accountAddress.addressLine1());
+            model.put("addressLine2", accountAddress.addressLine2());
+            model.put("postcode", accountAddress.postcode());
+        }
+
         return new ModelAndView("basket", model);
     }
 
--- src/main/java/com/cakefactory/repository/AccountAddressRepository.java
+++ src/main/java/com/cakefactory/repository/AccountAddressRepository.java
@@ -4,4 +4,6 @@ import com.cakefactory.model.AccountAddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountAddressRepository extends JpaRepository<AccountAddressEntity, String> {
+
+    AccountAddressEntity getAccountAddressByEmail(String email);
 }
