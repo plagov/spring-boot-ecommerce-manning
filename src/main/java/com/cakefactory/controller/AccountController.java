698439b6cc91280cffd96b1ed4f78df6da93b784
Vitali Plagov <vplagov@pm.me>
2025-04-13 23:37:09 +0300

    Edit account address
    Add new controller to open the edit form for account address.
    Add new controller to save updated address.
    Add new view for the edit form to update address.

--- src/main/java/com/cakefactory/controller/AccountController.java
+++ src/main/java/com/cakefactory/controller/AccountController.java
@@ -1,8 +1,11 @@
 package com.cakefactory.controller;
 
+import com.cakefactory.model.AccountAddress;
 import com.cakefactory.service.AccountAddressService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
@@ -27,5 +30,32 @@ public class AccountController {
         }
 
         return new ModelAndView("account", model);
+    }
+
+    @GetMapping("/account/edit")
+    public ModelAndView getEditAccountView(Map<String, Object> model, Principal principal) {
+        if (principal != null) {
+            var accountAddress = accountAddressService.getAccountAddress(principal.getName());
+            model.put("addressLine1", accountAddress.addressLine1());
+            model.put("addressLine2", accountAddress.addressLine2());
+            model.put("postcode", accountAddress.postcode());
+        }
+
+        return new ModelAndView("account-edit", model);
+    }
+
+    @PostMapping("/account")
+    public String updateAccountAddress(
+            @RequestParam String addressLine1,
+            @RequestParam String addressLine2,
+            @RequestParam String postcode,
+            Principal principal) {
+        if (principal != null) {
+            var accountEmail = principal.getName();
+            var updateAccountPayload = new AccountAddress(accountEmail, addressLine1, addressLine2, postcode);
+            accountAddressService.saveAccountAddress(updateAccountPayload);
+        }
+
+        return "redirect:/account";
     }
 }
